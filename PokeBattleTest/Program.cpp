c9081ed04e7ab82d2f68526e7213d6668d441c40
Benjamin Russell <40721667+russellpuff@users.noreply.github.com>
2022-11-16 01:50:36 -0500

    Add project files.

---
+++ PokeBattleTest/Program.cpp
@@ -0,0 +1,9 @@
+// Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Benjamin Russell
+// This project was made as CSCI201 
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; //test
+}
